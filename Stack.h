1df3f2ecf5881a98264b9b99dfbb5f1b589e7d85
nudtLu <1295470449@qq.com>
2019-12-01 21:19:50 +0800

    Add files via upload

---
+++ Stack.h
@@ -0,0 +1,78 @@
+template <class T>
+class Stack
+{
+private:
+	struct Node
+	{
+		T data;
+		Node* next;
+	};
+	Node* head;
+	Node* tail;
+	int num;
+public:
+	Stack();
+	~Stack();
+	void push_stack(T data);
+	T pop_stack();
+	bool isEmpty();
+	int size();
+	T top();
+};
+
+template<class T>
+Stack<T>::Stack()
+{
+	this->head = this->tail = new Node;
+	this->num = 0;
+}
+
+template<class T>
+Stack<T>::~Stack()
+{
+
+}
+
+template <class T>
+inline void Stack<T>::push_stack(T data)
+{
+	this->num++;
+	Node* node = new Node;
+	node->data = data;
+	node->next = this->head;
+	if (this->head->next == NULL)
+		node->next = NULL;
+	this->head = node;
+}
+
+template<class T>
+inline T Stack<T>::pop_stack()
+{
+	this->num--;
+	T ans = this->head->data;
+	Node* temp = this->head;
+	this->head = this->head->next;
+	delete temp;
+	return ans;
+}
+
+template<class T>
+inline bool Stack<T>::isEmpty()
+{
+	if (this->num == 0)
+		return true;
+	else
+		return false;
+}
+
+template<class T>
+inline int Stack<T>::size()
+{
+	return this->num;
+}
+
+template<class T>
+inline T Stack<T>::top()
+{
+	return this->head->data;
+}
