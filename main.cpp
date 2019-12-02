5476bc1a1d1f7c12be4756ef8727149ce1b039eb
printfLcc <1295470449@qq.com>
2019-12-02 12:39:54 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+
+void allSort(int arr[], int step, int size, int list[], bool book[]);
+int main()
+{
+    int n;
+    cin >> n;
+
+    int *nums = new int[n];
+    int *list = new int[n];
+    bool *book = new bool[n];
+
+    for (int i = 0; i < n; i++)
+        list[i] = book[i] = 0;
+
+    for (int i = 0; i < n; i++)
+        cin >> nums[i];
+
+    allSort(nums, 0, n, list, book);
+    delete[] nums;
+    delete[] list;
+    delete[] book;
+    return 0;
+}
+
+void allSort(int arr[], int step, int size, int list[], bool book[])
+{
+    if (step == size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << list[i] << " ";
+        }
+        cout << endl;
+        return;
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        if (!book[i])
+        {
+            list[step] = arr[i];
+            book[i] = true;
+            allSort(arr, step + 1, size, list, book);
+            book[i] = false;
+        }
+    }
+}
