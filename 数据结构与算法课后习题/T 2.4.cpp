e5025f11b29f66d885145e24172eb918816fd275
printfLcc <1295470449@qq.com>
2019-12-06 21:35:57 +0800

    Create T 2.4.cpp

---
+++ 数据结构与算法课后习题/T 2.4.cpp
@@ -0,0 +1,17 @@
+//Programmer: Luchangcheng  2019/12/6
+//Compiler: MSVC14.23  tested on Visual Studio 2019
+//题目：编写将线性表原地置逆的算法，空间开销为O(1)
+template<class T>
+void reverseVector(vector<T>& list, int n)
+{
+	if (list.size() < n)
+		return;
+
+	int i = 0;
+	for (int i = 0; i < n / 2; i++)
+	{
+		int temp = list[i];
+		list[i] = list[n - 1 - i];
+		list[n - 1 - i] = temp;
+	}
+}
