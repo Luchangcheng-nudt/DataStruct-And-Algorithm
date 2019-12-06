017d75640c55d370759f39a1d5bb0465c1f96f18
printfLcc <1295470449@qq.com>
2019-12-06 21:38:43 +0800

    Create T 2.6.cpp

---
+++ 数据结构与算法课后习题/T 2.6.cpp
@@ -0,0 +1,103 @@
+//Program by Luchangcheng  2019/12/6
+//Compiler: MSVC14.23  tested on Visual Studio 2019
+//题目：将中缀表达式转换成后缀表达式的算法
+#include <iostream>
+#include <stack>
+#include <map>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	map<char, int>code;
+	code['+'] = code['-'] = 1;
+	code['*'] = code['/'] = 2;
+	string mid;
+	cin >> mid;
+	stack<char>s;
+	vector<string>ans;
+
+	int i = 0;
+	if (mid[i] == '+' || mid[i] == '-')
+	{
+		string temp = "";
+		temp += mid[i];
+		while ('0' <= mid[++i] && mid[i] <= '9')
+			temp += mid[i];
+		ans.push_back(temp);
+	}
+	for (i; i < mid.length();)
+	{
+		if ('0' <= mid[i] && mid[i] <= '9')
+		{
+			string temp = "";
+			while ('0' <= mid[i] && mid[i] <= '9' || mid[i] == '.')
+			{
+				temp += mid[i];
+				i++;
+			}
+			ans.push_back(temp);
+		}
+		else
+		{
+			if (mid[i] == '(')
+			{
+				s.push(mid[i]);
+				i++;
+			}
+			else if (mid[i] == ')')
+			{
+				while (s.top() != '(')
+				{
+					string temp = "";
+					temp += s.top();
+					ans.push_back(temp);
+					s.pop();
+				}
+				s.pop();
+				i++;
+			}
+			else
+			{
+				if ((mid[i] == '-') && !('0' <= mid[i - 1] && mid[i - 1] <= '9'))
+				{
+					string temp = "-";
+					i++;
+					while ('0' <= mid[i] && mid[i] <= '9' || mid[i] == '.')
+					{
+						temp += mid[i];
+						i++;
+					}
+					ans.push_back(temp);
+				}
+				else
+				{
+					while (!s.empty() && code[mid[i]] <= code[s.top()])
+					{
+						string temp = "";
+						temp += s.top();
+						ans.push_back(temp);
+						s.pop();
+					}
+					s.push(mid[i]);
+					i++;
+				}
+			}
+		}
+	}
+
+	while (!s.empty())
+	{
+		string temp = "";
+		temp += s.top();
+		s.pop();
+		ans.push_back(temp);
+	}
+
+	int size = ans.size();
+	for (i = 0; i < size - 1; i++)
+		cout << ans[i];
+	cout << ans[size - 1];
+
+	return 0;
+}
