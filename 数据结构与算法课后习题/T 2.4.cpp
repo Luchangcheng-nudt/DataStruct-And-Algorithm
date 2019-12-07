27314f9b16112551529bb70f356ce308e0f50a2b
printfLcc <1295470449@qq.com>
2019-12-07 23:20:03 +0800

    Update T 2.4.cpp

--- 数据结构与算法课后习题/T 2.4.cpp
+++ 数据结构与算法课后习题/T 2.4.cpp
@@ -1,5 +1,5 @@
 //Programmer: Luchangcheng  2019/12/6
-//Compiler: MSVC14.23  tested on Visual Studio 2019
+//Compiler: MSVC14.23  tested on Visual Studio 2019   --std = C++17
 //题目：编写将线性表原地置逆的算法，空间开销为O(1)
 template<class T>
 void reverseVector(vector<T>& list, int n)
