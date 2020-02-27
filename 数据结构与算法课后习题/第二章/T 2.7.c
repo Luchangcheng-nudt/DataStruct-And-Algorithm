b484c6b09c755d95000b5623b5cfdf7715075b20
printfLcc <1295470449@qq.com>
2020-02-27 17:37:37 +0800

    Update T 2.7.c

--- 数据结构与算法课后习题/第二章/T 2.7.c
+++ 数据结构与算法课后习题/第二章/T 2.7.c
@@ -1,20 +1,21 @@
-//Programer: Luchangcheng  2019/12/8
-//Compiler: gcc version 4.9.2 (x86_64-posix-seh-rev1, Built by MinGW-W64 project)  tested on Ubuntu Kylin 19.10.1
-int match(char line[])
+int match(char s[])
 {
-    int length = strlen(line);
-    int flag = 0;
+    char stack[128] = {0};
+    int cur = -1;
+    int length = strlen(s);
+
     for (int i = 0; i < length; i++)
     {
-        if (line[i] == '(')
-            flag = 1;
-        if (line[i] == ')')
+        if (s[i] == '(')
+            stack[++cur] = s[i];
+        if (s[i] == ')')
         {
-            if (flag == 1)
-                flag = 0;
+            if (cur != -1 && stack[cur] == '(')
+                stack[cur--] = 0;
             else 
                 return 0;
         }
     }
-    return !flag;
+
+    return cur == -1;
 }
