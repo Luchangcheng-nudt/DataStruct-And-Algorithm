3371ca05c7142e6cd830490725b0bed66b80d49f
Luchangcheng <1295470449@qq.com>
2020-03-23 07:34:02 +0800

    Update QSortTemplate.c

--- 数据结构与算法课后习题/第五章/QSortTemplate.c
+++ 数据结构与算法课后习题/第五章/QSortTemplate.c
@@ -1,23 +1,24 @@
-void QuickSort(int data[], int left, int right)
+void QSort(int data[], int left, int right)
 {
-	if (left >= right)
-		return;
-	int i = left;
-	int j = right;
-	int temp = data[i];
+    if (left >= right)
+        return;
+    int i = left;
+    int j = right;
+    int temp = data[i];
 
-	if (i < j)
-	{
-		while (i < j && temp < data[j])
-			j--;
-		data[i] = data[j];
-
-		while (i < j && temp >= data[i]);
-			i++;
-		data[j] = data[i];
-
-		data[i] = temp;
-		QuickSort(data, left, i - 1);
-		QuickSort(data, i + 1, right);
-	}
+    while (i < j)
+    {
+        while (i < j && temp < data[j])
+            j--;
+        if (i < j)
+            data[i++] = data[j];
+        
+        while (i < j && temp >= data[i])
+            i++;
+        if (i < j)
+            data[j--] = data[i];
+    }
+    data[i] = temp;
+    QSort(data, left, i - 1);
+    QSort(data, i + 1, right);
 }
